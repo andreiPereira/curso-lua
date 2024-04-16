084c543255d53d9ae7d451bf76b87271e33686aa
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2024-04-16 17:26:41 -0300

    Inicio

---
+++ Aulas.lua
@@ -0,0 +1,13 @@
+pessoa = {}
+
+pessoa.nome = "Andrei"
+pessoa.idade = 25
+pessoa.cidade = "Ibiruba"
+
+for chave, valor in pairs(pessoa) do
+    print(valor)
+end
+
+saudacoes = {'oi', 'ola', 'tudo bem', 'hello', 'opa'};
+
+--print(pessoa.nome)
